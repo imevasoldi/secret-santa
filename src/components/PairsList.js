7599524bf0daf7e19fffc2dbc0a1de7be7c74f0b
imevasoldi <imevasoldi@gmail.com>
2022-11-18 10:50:07 -0600

    render random pair list avoiding self assignation

--- src/components/PairsList.js
+++ src/components/PairsList.js
@@ -8,12 +8,22 @@ const PairsList = (props) => {
   props.persons.sort(() => 0.5 - Math.random())
   
   const matches = props.persons.map((person, index) => {
+    let stop = props.persons.length-1
+    let pair
+    console.log("stops", index, stop, pair)
+    
+    if (index === stop){
+      pair = 0
+    }else {
+      pair = index+1
+    }
+
     return {
         ...person,
         santa: person.name,
-        gifted: props.persons[2].name
+        gifted: props.persons[pair].name
       }
-  })
+    })
 
   const renderPairsList = matches.map((match, i) => {
     return (
